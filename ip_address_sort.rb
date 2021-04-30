150aeba6bbe2e3abb1f9cb90faa7d1acc0df6932
Jeff Sutherland <jeff@sutherland.codes>
2021-04-30 15:41:12 -0600

    Fixes the counting bug

--- ip_address_sort.rb
+++ ip_address_sort.rb
@@ -1,44 +1,47 @@
 class IpAddressSort
   attr_accessor :ip_ranking
+  attr_accessor :ip_addresses
 
   def initialize
     @ip_ranking = {}
+    @ip_addresses = {}
   end
 
   def request_handled(ip_address)
-    val = 1
+    cnt = 1
 
-    if @ip_ranking[val]
-      if @ip_ranking[val - 1]
-        @ip_ranking[val - 1].delete(ip_address)
-      end
-    else 
-      @ip_ranking[val] = []
+    if @ip_addresses[ip_address]
+      cnt = @ip_addresses[ip_address]
+      cnt = cnt + 1
     end
+
+    @ip_addresses[ip_address] = cnt
+
+    top_100_counts = @ip_ranking.keys
+    top_100_min_key = 0
+
+    if top_100_counts.length > 100
+      top_100_min_key = top_100_counts.min
+      @ip_ranking.delete(top_100_min_key)
+    end
+
+    if cnt > top_100_min_key
+      unless @ip_ranking[cnt] 
+        @ip_ranking[cnt] = []
+      end
       
-    @ip_ranking[val].push(ip_address)
+      if @ip_ranking[cnt - 1]
+        cnts = @ip_ranking[cnt - 1]
+        cnts.delete(ip_address)
+        @ip_ranking[cnt - 1] = cnts
+      end
+    
+      @ip_ranking[cnt].push(ip_address)
+    end
   end
 
   def top100
-    rankings = @ip_ranking.keys.sort do |a, b|
-      b <=> a
-    end
-
-    key_rankings_100 = rankings[0..99]
-    ip_rankings = []
-
-    key_rankings_100.each do |ranking|
-      if ip_rankings.length <= 100 # skip all this logic if ip_rankings already has 100
-        ips = @ip_ranking[ranking]
-        ips.each do |ip|
-          if ip_rankings.length <= 100 # because we are adding groups of IPs, if a set of IPs pushes the array over 100, we need to stop
-            ip_rankings.push(ip)
-          end
-        end
-      end
-    end
-
-    return ip_rankings
+    sorted_rankings = Hash[@ip_ranking.sort_by { |k, v| k }]
   end
 
   def clear
@@ -59,6 +62,42 @@ random_ip_addresses = []
   random_ip_addresses.push("#{ip_1}.#{ip_2}.#{ip_3}.#{ip_4}")
 end
 
+10.times do
+  random_ip_addresses.push("10.10.10.10")
+end
+
+9.times do
+  random_ip_addresses.push("9.9.9.9")
+end
+
+8.times do
+  random_ip_addresses.push("8.8.8.8")
+end
+
+7.times do
+  random_ip_addresses.push("7.7.7.7")
+end
+
+6.times do
+  random_ip_addresses.push("6.6.6.6")
+end
+
+5.times do
+  random_ip_addresses.push("5.5.5.5")
+end
+
+4.times do
+  random_ip_addresses.push("4.4.4.4")
+end
+
+3.times do
+  random_ip_addresses.push("3.3.3.3")
+end
+
+2.times do
+  random_ip_addresses.push("2.2.2.2")
+end
+
 random_ip_addresses.each do |ip|
   ip_sorter.request_handled(ip)
 end
@@ -68,6 +107,6 @@ puts ip_sorter.top100.inspect
 
 ip_sorter.clear
 
-puts ip_sorter.ip_addresses.inspect
 puts ip_sorter.top100.inspect
+
 
